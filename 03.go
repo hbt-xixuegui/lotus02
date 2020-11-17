fe758b59bb453f27103ec3735af87ef18d7e09d0
sansi2020 <465620111@qq.com>
2020-11-17 17:38:02 +0800

    03

---
+++ 03.go
@@ -0,0 +1,22 @@
+module github.com/filecoin-project/lotus
+
+go 1.14
+
+require (
+	contrib.go.opencensus.io/exporter/jaeger v0.1.0
+	contrib.go.opencensus.io/exporter/prometheus v0.1.0
+	github.com/BurntSushi/toml v0.3.1
+	github.com/GeertJohan/go.rice v1.0.0
+	github.com/Gurpartap/async v0.0.0-20180927173644-4f7f499dd9ee
+	github.com/Shopify/sarama v1.19.0
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d
+	github.com/bsm/sarama-cluster v2.1.15+incompatible
+	github.com/buger/goterm v0.0.0-20200322175922-2f3e71b85129
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e
+	github.com/cockroachdb/pebble v0.0.0-20201001221639-879f3bfeef07
+	github.com/coreos/go-systemd/v22 v22.0.0
+	github.com/dchest/blake2b v1.0.0
+	github.com/detailyang/go-fallocate v0.0.0-20180908115635-432fa640bd2e
+	github.com/dgraph-io/badger/v2 v2.2007.2
+	github.com/docker/go-units v0.4.0
